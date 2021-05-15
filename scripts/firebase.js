20c95f3fbb61bce01f7a88d729aa1a94f7fb681e
NicolasMonteroM <nicolasmontero42@gmail.com>
2021-05-15 17:18:04 -0500

    Initial commit

---
+++ scripts/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAYSrJsFCZDS4Gbe9X7OghSw_DHONyI3eo",
+    authDomain: "planner-eco.firebaseapp.com",
+    databaseURL: "https://planner-eco-default-rtdb.firebaseio.com",
+    projectId: "planner-eco",
+    storageBucket: "planner-eco.appspot.com",
+    messagingSenderId: "58431883274",
+    appId: "1:58431883274:web:83f0763af33766d523f7f3",
+    measurementId: "G-F0G1T5ZWGF"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ scripts/planner.js
@@ -0,0 +1,8 @@
+var database = firebase.database();
+
+const task = document.getElementById('taskInfo');
+const addBtn = document.getElementById('addTask');
+
+//const toDoContainer = document.getElementById('toDoContainer');
+//const doingContainer = document.getElementById('doingContainer');
+//const doneContainer = document.getElementById('doneContainer');
